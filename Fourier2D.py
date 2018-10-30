f9500075fbe479be57bdb72245e138ec22a77131
Juan Sebastian Martinez Acevedo <42219672+jusebastianm@users.noreply.github.com>
2018-10-30 10:03:17 -0500

    Fourier 2D completo

--- Fourier2D.py
+++ Fourier2D.py
@@ -8,7 +8,6 @@
 import numpy as np
 import matplotlib.pylab as plt
 from scipy import fftpack
-from scipy.signal import convolve2d
 
 #_______________________________________________________________________________________________________________________________________
 #_______________________________________________________________________________________________________________________________________
@@ -29,10 +28,62 @@ log_AFS= np.log(abs_FT_shift)
 plt.figure()
 plt.title("Transformada de Fourier")
 plt.imshow(log_AFS)
-plt.savefig("Transfor")
+plt.savefig("MartinezSebastian_FT2D.pdf")
 
 #_______________________________________________________________________________________________________________________________________
 #_______________________________________________________________________________________________________________________________________
+
+def maximo(FT_shift):
+	temp=0
+	for i in range (np.shape(FT_shift)[0]):		
+		for j in range (np.shape(FT_shift)[0]):
+			if temp<FT_shift[i][j]:
+				temp=FT_shift[i][j]
+				
+	return temp
+
+pru=maximo(FT_shift)
+print pru
+
+
+def filtro(maximo,minimo,FT_shift):
+	for i in range (np.shape(FT_shift)[0]):		
+		for j in range (np.shape(FT_shift)[0]):
+			if (FT_shift[i][j]>minimo and FT_shift[i][j]<maximo):
+				FT_shift[i][j]=0
+			else:
+				FT_shift[i][j]=FT_shift[i][j]
+
+	return FT_shift
+
+
+filtrada=filtro(pru,2200,FT_shift)
+FT_shift=fftpack.ifftshift(filtrada)
+inv_filtrada=fftpack.ifft2(FT_shift)
+
+
+
+plt.figure()
+plt.title("Imagen filtrada")
+plt.imshow(log_AFS-np.log(abs(filtrada)))
+plt.savefig("MartinezSebastian_FT2D_filtrada.pdf")
+
+
+plt.figure()
+plt.title("Imagen filtrada")
+plt.imshow(abs(inv_filtrada))
+plt.savefig("MartinezSebastian_Imagen_filtrada.pdf")
+
+
+
+
+#_______________________________________________________________________________________________________________________________________
+#_______________________________________________________________________________________________________________________________________
+
+
+
+
+
 
 
 
